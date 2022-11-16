6720284dcb6cf7b8d6f74ef571d30b95e36f57ee
grinchmachine <118330107+grinchmachine@users.noreply.github.com>
2022-11-16 18:25:05 -0500

    Update the_magic.js

--- the_magic.js
+++ the_magic.js
@@ -1,10 +1,10 @@
-const codes = {
-    'greenMeanie' : 'almost',
-    'Password123' : 'yeh'
-    'chrisms' : 'what are you, a hacker? Try harder'
-};
-
 function checkPassword() {
+    const codes = {
+        'greenMeanie' : 'almost',
+        'Password123' : 'yeh'
+        'chrisms' : 'what are you, a hacker? Try harder'
+    };
+    
     const santa_letter = codes[$('#mailbox').val()];
     $('#message').val(santa_letter ? santa_letter : 'error');
 }
