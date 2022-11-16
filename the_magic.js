a2396652bc18c0ce2b106855bb6713071cf4e13d
grinchmachine <118330107+grinchmachine@users.noreply.github.com>
2022-11-16 17:59:01 -0500

    Update the_magic.js

--- the_magic.js
+++ the_magic.js
@@ -2,4 +2,6 @@ function checkPassword() {
     console.log('Ooooo');
     const passcode = document.getElementById('mailbox').value;
     console.log('passcode',passcode);
+    const passcodeJQuery = $('#mailbox').val();
+    console.log('try jquery?',passcodeJQuery);
 }
