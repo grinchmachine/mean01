07ad46b8128be8e22edb0bc31ce98e69a9d2bd37
grinchmachine <118330107+grinchmachine@users.noreply.github.com>
2022-12-09 18:53:13 -0500

    Update the_magic.js

--- the_magic.js
+++ the_magic.js
@@ -18,7 +18,7 @@ const codes = {
 
 function flashMsg(status) {
        let msg = status ? 'CORRECT' : 'WRONG PASSWORD, SUCKER!';
-       let colour = status ? '#05CB14' : ':#EE0A06';
+       let colour = status ? '#05CB14' : '#EE0A06';
        $('#status').html(`<span style="color: ${colour}">${msg}</span>`);
        $('#status').fadeIn(20);
        setTimeout(()=>{
@@ -34,11 +34,13 @@ function checkPassword() {
        $('#message_block').html(santa_letter ? santa_letter : main_message);
        $('#message_block').addClass('typed');
        if(santa_letter == 'greenmeanie') {
+              $('#extra_border').fadeIn(20);
               $('#extra').html('<div style="display:inline-block;text-align:left;width:300px;"><a style="color:#444" href="https://www.jigidi.com/solve/azs8pria/m-e-a-n-01-encryption/?utm_source=em">M.E.A.N.01 Encryption<img style="display:block;border:none;margin:5px 0 0 0;" src="//thumbs.jigidi.com/embed.php?t=m_AZS8PRIA_77" alt="Jigsaw puzzle - M.E.A.N.01 Encryption"></a></div>');
        } else if (santa_letter == 'password123') {
               alert('password123')
               $('#extra').html('<iframe width="560" height="315" src="https://www.youtube.com/embed/dJwSjrIFw8U" title="You Did It!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
        } else {
+              $('#extra_border').fadeOut(20);
               $('#message_block').removeClass('typed');
        }
 }
