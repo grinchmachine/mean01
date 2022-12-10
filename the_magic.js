2ad645a83d3b2bace76f799e8698b6723abaebaf
grinchmachine <118330107+grinchmachine@users.noreply.github.com>
2022-12-09 21:16:49 -0500

    Update the_magic.js

--- the_magic.js
+++ the_magic.js
@@ -40,11 +40,12 @@ function checkPassword() {
               }, 6610);
        } else if (response == 'password123') {
               alert('password123_debug_0.5');
-              $('.typed').css("-webkit-animation-play-state", "paused");
+              $('.typed').css("width","12.5%");
               $('#extra_border').fadeIn(20);
               $('#extra').html('<iframe width="560" height="315" src="https://www.youtube.com/embed/dJwSjrIFw8U?autoplay=1&cc_load_policy=1&rel=0&showinfo=0" title="You Did It!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
        } else {
               alert('not the right answer');
+              $('.typed').css("width","100%");
               $('#extra_border').fadeOut(20);
               $('#message_block').removeClass('typed');
        }
